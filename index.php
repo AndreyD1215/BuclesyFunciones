b1a099d1f8fa5e299d4ea9d3eb51b7f6687ab10c
AndreyD1215 <61071075+AndreyD1215@users.noreply.github.com>
2020-02-28 19:27:37 -0500

    Se codifica arreglo indexado y arreglo asociativo

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+    
+    echo("clase 4 arreglos, arreglos y funciones");
+    //array indexado
+    $arreglo=array("Juan","Maria","James","Sandra");
+    echo("<br>");
+    print_r($arreglo);
+
+    //array asociativo
+    $arreglo=array('usuario1'=>"Juan",'usuario2'=>"Maria",'usuario3'=>"James",'usuario4'=>"Sandra");
+    echo("<br>");
+    print_r($arreglo);
+    
+?>
